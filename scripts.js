78e7c5ed31f98665f47631c99755910548f442f8
WenddylReis <wamdreis@gmail.com>
2024-08-19 13:38:38 -0300

    Novas imagens

--- scripts.js
+++ scripts.js
@@ -2,9 +2,15 @@ function generateNumber() {
     const min = Math.ceil(parseFloat(document.querySelector(".input-min").value));
     const max = Math.floor(parseFloat(document.querySelector(".input-max").value));
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-
+    if (min >= max) {
+        alert("O valor minimo tem que ser ( menor ) que o valor máximo")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
     alert(result);
 }
+
+    }
+
+    
 
 
